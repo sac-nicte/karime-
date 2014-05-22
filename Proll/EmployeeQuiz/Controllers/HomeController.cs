482e12c2a1104bf3071ef05cebfa4a0934e081f5
sac-nicte <karymnict@gmail.com>
2014-05-21 20:41:59 -0500

    Recover
    recuperacion

---
+++ Proll/EmployeeQuiz/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using EmployeeQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeQuiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index(Employe emp)
+        {
+
+            //Creo el objeto del modelo de datos
+            Payrolldm nomina = new Payrolldm(
+                @"C:\Users\vladimir\Desktop\karime\Proll\EmployeeQuiz\Models\EmployeeDb.csv");
+
+            //busco el empleado dado su id
+            var empleado = nomina.GetEmployeById(emp.Id);
+
+
+
+            return View("WorkerView", empleado);
+        }
+
+
+
+
+    }
+}
---
+++ Proll/EmployeeQuiz/Models/Payrolldm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+//Espacio de nombre para espacio de nombres
+using System.IO;
+using System.Linq;
+using System.Web;
+
+
+namespace EmployeeQuiz.Models
+{
+    public class Payrolldm
+    {
+        //lista de empleados llevan <> (plantillas) para especificar que tipo de dato contendra la lista
+        List<Employe> empList;
+
+
+        //Constructor del modelo 
+        public Payrolldm(string dbPath)
+        {
+           
+            //Creando el objeto de la lista empleados
+            empList = new List<Employe>();
+            
+            //Leer el Archivo             
+            var reader = new StreamReader(File.OpenRead(dbPath));
+
+            //Parsear
+            while (!reader.EndOfStream)
+            { 
+                //Leo una linea
+                var line = reader.ReadLine();
+
+                //Pasear
+                //Separar los valores
+                //Guardar un objeto
+
+                var valores = line.Split(',');
+                
+                empList.Add(new Employe
+                    {
+                        Id = valores[0],
+                        FirstLastname = valores[1],
+                        SecondLastname = valores[2],
+                        Name = valores[3],
+                        Position = valores[4],
+                        Wage = double.Parse(valores[5]),
+                        Sex = char.Parse(valores[6]),
+                        PhothoPath = valores[7]
+                    });
+                }
+        }
+
+
+        //accesor
+        public Employe GetEmployeById(string id)
+        {
+            var emp = empList.Find(e => e.Id == id);
+                return emp;
+        }
+    }
+}
